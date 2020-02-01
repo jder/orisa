8ec01e69d4c7a92a2adc8f0b486c141573981419
Jesse Rusak <me@jesserusak.com>
2020-02-01 15:48:49 -0500

    autoscroll

--- client/src/ChatPane.tsx
+++ client/src/ChatPane.tsx
@@ -1,14 +1,20 @@
-import React, { } from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
+import autoscroll from 'autoscroll-react';
+
 import { ServerMessage } from './Messages';
 import ChatPaneMessage from './ChatPaneMessage';
 
-const ChatPane = (props: { messages: ServerMessage[] }) => {
+class ChatPane extends React.Component<{messages: ServerMessage[]}> {
 
-  return (
-    <div className="ChatPane">
-      {props.messages.map((m) => <ChatPaneMessage message={m} key={m.id} />)}
-    </div>
-  );
+  render() {
+    const { messages, ...props } = this.props;
+
+    return (
+      <div className="ChatPane" { ...props } >
+        {messages.map((m) => <ChatPaneMessage message={m} key={m.id} />)}
+      </div>
+    );
+  }
 }
 
-export default ChatPane;
+export default autoscroll(ChatPane, {isScrolledDownThreshold: 10});
