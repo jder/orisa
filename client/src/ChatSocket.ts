943d4d26d2760da49ff65bd556092e99e0ce927b
Jesse Rusak <me@jesserusak.com>
2020-03-01 14:59:47 -0500

    Reconnect immediately when you try to send a message

--- client/src/ChatSocket.ts
+++ client/src/ChatSocket.ts
@@ -7,7 +7,8 @@ export class ChatSocket {
   private url: string;
   private ws: WebSocket;
   private username: string;
-  private next_timeout: number;
+  private next_delay: number;
+  private reconnect_timeout: NodeJS.Timeout | undefined;
 
   onmessage?: (message: ToClientMessage) => void;
   buffer: ToServerMessage[] = [];
@@ -16,10 +17,15 @@ export class ChatSocket {
     this.url = url;
     this.ws = this.connect();
     this.username = username;
-    this.next_timeout = MIN_TIMEOUT;
+    this.next_delay = MIN_TIMEOUT;
   }
 
   private connect() {
+    if (this.reconnect_timeout) {
+      clearTimeout(this.reconnect_timeout);
+      this.reconnect_timeout = undefined;
+    }
+
     this.ws = new WebSocket(this.url);
     this.ws.onmessage = (event: MessageEvent) => {
       if (this.onmessage) {
@@ -32,12 +38,12 @@ export class ChatSocket {
       console.error("websocket error", event);
     }
     this.ws.onclose = (event: CloseEvent) => {
-      setTimeout(() => this.connect(), this.next_timeout);
-      this.next_timeout = Math.min(this.next_timeout * 2, MAX_TIMEOUT);
+      this.reconnect_timeout = setTimeout(() => this.connect(), this.next_delay);
+      this.next_delay = Math.min(this.next_delay * 2, MAX_TIMEOUT);
     }
     this.ws.onopen = (event: Event) => {
       console.debug("websocket open");
-      this.next_timeout = MIN_TIMEOUT;
+      this.next_delay = MIN_TIMEOUT;
 
       this.send(new LoginMessage(this.username));
 
@@ -55,6 +61,12 @@ export class ChatSocket {
     if (this.ws.readyState !== WebSocket.OPEN) {
       console.warn("Tried to send message when websocket is closed; buffering");
       this.buffer.push(message);
+      if (this.reconnect_timeout) {
+        // we're waiting to reconnect; trigger immediately instead since this is common
+        // when developing locally and restarting the server after a long offline period
+        console.info("Trying immediate reconnect.");
+        this.connect();
+      }
     } else {
       console.debug("Sending message", message);
       this.ws.send(JSON.stringify(message));
